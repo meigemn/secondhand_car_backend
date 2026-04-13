6db7bedd7d569e7a019c79e2c1b9f581d378a809
meigemn <carloscc9297@gmail.com>
2026-04-13 16:04:20 +0200

    :sparkles: new UserDto on Dtos->EntityDtos

---
+++ secondhand_car_backend/Models/Dtos/EntityDtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace secondhand_car_backend.Models.Dtos.EntityDtos
+{
+    public class UserDto
+    {
+
+        public string Id { get; set; } = string.Empty;
+
+        public string? UserName { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
