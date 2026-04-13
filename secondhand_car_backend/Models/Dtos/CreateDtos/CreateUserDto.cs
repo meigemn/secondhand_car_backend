e76ac873ad51a1b1454dd54840198ed73e74db99
meigemn <carloscc9297@gmail.com>
2026-04-13 16:01:08 +0200

    :sparkles: new CreateUserDto on Dtos

---
+++ secondhand_car_backend/Models/Dtos/CreateDtos/CreateUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace secondhand_car_backend.Models.Dtos.CreateDtos
+{
+    public class CreateUserDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
