f60518adef2c01783a77f4aba77c85d942244a91
meigemn <carloscc9297@gmail.com>
2026-04-13 16:14:41 +0200

    :adhesive_bandage: LoginRequestDto now ask for UserName instead of Email

--- secondhand_car_backend/Models/Dtos/RequestDtos/LoginRequestDto.cs
+++ secondhand_car_backend/Models/Dtos/RequestDtos/LoginRequestDto.cs
@@ -2,7 +2,7 @@
 {
     public class LoginRequestDto
     {
-        public string? Email { get; set; }
+        public string? UserName { get; set; }
         public string? Password { get; set; }
         public string? TwoFactorCode { get; set; }
         public string? TwoFactorRecoveryCode { get; set; }
