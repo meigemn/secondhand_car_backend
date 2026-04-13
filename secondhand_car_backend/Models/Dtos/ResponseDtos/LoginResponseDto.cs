751f1fea027351080102858073a6e73233051056
meigemn <carloscc9297@gmail.com>
2026-04-13 16:16:28 +0200

    :ambulance: now it doesn't return password

--- secondhand_car_backend/Models/Dtos/ResponseDtos/LoginResponseDto.cs
+++ secondhand_car_backend/Models/Dtos/ResponseDtos/LoginResponseDto.cs
@@ -6,9 +6,7 @@
         public string Message { get; set; } = string.Empty;
         public string UserId { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
-        public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string Token { get; set; } = string.Empty;
         public List<string> Roles { get; set; } = new List<string>();
