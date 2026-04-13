4c64f840267abd90ea291ba46e17b74a49296a45
meigemn <carloscc9297@gmail.com>
2026-04-13 16:07:23 +0200

    :sparkles: new LoginResponseDto on Dtos->ResponseDtos

---
+++ secondhand_car_backend/Models/Dtos/ResponseDtos/LoginResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace secondhand_car_backend.Models.Dtos.ResponseDtos
+{
+    public class LoginResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
