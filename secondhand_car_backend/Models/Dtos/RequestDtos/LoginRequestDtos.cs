082a6bc2277a0336ea6bfe0e56fe1e598a263a79
meigemn <carloscc9297@gmail.com>
2026-04-13 16:05:46 +0200

    :sparkles: new LoginRequestDto on Dtos->RequestDtos

---
+++ secondhand_car_backend/Models/Dtos/RequestDtos/LoginRequestDtos.cs
@@ -0,0 +1,10 @@
+﻿namespace secondhand_car_backend.Models.Dtos.RequestDtos
+{
+    public class LoginRequestDtos
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? TwoFactorCode { get; set; }
+        public string? TwoFactorRecoveryCode { get; set; }
+    }
+}
