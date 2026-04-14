3a6179699514bf1d0ed573a8e4722f51bb255aa2
meigemn <carloscc9297@gmail.com>
2026-04-14 15:50:33 +0200

    :sparkles: Added UnitsOfWork on Models-> UnitsOfWork

---
+++ secondhand_car_backend/Models/UnitsOfWork/MeigemnUnitsOfWork.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using secondhand_car_backend.Models.Context;
+using secondhand_car_backend.Entities;
+using secondhand_car_backend.Models.Context;
+using secondhand_car_backend.Models.Repositories;
+using System;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace secondhand_car_backend.Models.UnitsOfWork
+{
+    public sealed class MeigemnUnitOfWork : IDisposable
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger _logger;
+        private readonly MeigemnDbContext _context;
+
+        // Repositorios
+        private MeigemnRepository<IdentityUser>? _usersRepository;
+ 
+
+        // Propiedad de acceso a la base de datos
+        public Microsoft.EntityFrameworkCore.Infrastructure.DatabaseFacade Database => _context.Database;
+
+        // Constructor
+        public MeigemnUnitOfWork(MeigemnDbContext context, IServiceProvider serviceProvider, ILogger logger)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        // --- Repositorios ---
+        public MeigemnRepository<IdentityUser> Users => _usersRepository ??= GetRepository<IdentityUser>();
+
+
+        // --- Métodos de Transacción y Guardado ---
+
+        /// <summary>
+        /// Inicia una nueva transacción de base de datos especificando el Nivel de Aislamiento.
+        /// </summary>
+
+        public Task<IDbContextTransaction> BeginTransactionAsync(IsolationLevel isolationLevel)
+        {
+            // Usamos CancellationToken.None para satisfacer la sobrecarga de tres argumentos requerida por EF Core:
+            // BeginTransactionAsync(IsolationLevel, CancellationToken)
+            return _context.Database.BeginTransactionAsync(isolationLevel, CancellationToken.None);
+        }
+
+        public async Task Complete()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        // --- Método Auxiliar ---
+        private MeigemnRepository<T> GetRepository<T>() where T : class
+        {
+            return _serviceProvider.GetRequiredService(typeof(MeigemnRepository<T>)) as MeigemnRepository<T>;
+        }
+
+        // --- Implementación IDisposable ---
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
