ec01aefcb89fe2e5800f02303e3681c799aa83bf
meigemn <carloscc9297@gmail.com>
2026-04-14 15:51:07 +0200

    :sparkles: Added Utils

---
+++ secondhand_car_backend/Utils/Enums.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Json.Serialization;
+
+namespace secondhand_car_backend.Utils
+{
+
+    /// <summary>
+    ///     Tipos de orden
+    /// </summary>
+    public enum OrderType
+    {
+        up = 0,
+        down = 1
+    }
+
+    /// <summary>
+    ///     Tipos de filtrado para las tablas
+    /// </summary>
+    public enum FilterType
+    {
+        contains,
+        equals,
+        greatherThan,
+        greatherThanEqual,
+        isNullOrEmpty,
+        lessThan,
+        lessThanEqual,
+        notEquals
+    }
+
+    /// <summary>
+    ///		Especificación de los códigos de error
+    /// </summary>
+    public enum ResponseCodes
+    {
+
+        Ok = 0,
+        
+        ErrorToken = 1,
+        
+		OriginAccess = 2,
+        
+		InvalidUserName = 3,
+        
+        InvalidPassword = 4,
+        
+        BdConectionFailed = 5,
+        
+        NoDataFound = 6,
+        
+        SaveDataFailed = 7,
+        
+        SinEmpresa = 8,
+        
+        AccesoNoAutorizado = 9,
+        
+        DataError = 10,
+       
+        OtherError = 11,
+      
+        InvalidModel = 12,
+        
+        InvalidAccessType = 13,
+   
+        InvalidToken = 14
+    }
+
+    
+    
+}
---
+++ secondhand_car_backend/Utils/ModelAtributes.cs
@@ -0,0 +1,74 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace secondhand_car_backend.Utils
+{
+    public class ModelAttributes
+    {
+        /// <summary>
+        ///     Atributo de campo de modelo con los posibles tipos de filtrado que tiene
+        /// </summary>
+        public class FiltersAttribute : Attribute
+        {
+            /// <summary>
+            ///     Tipos de filtros aceptados
+            /// </summary>
+            public FilterType[] FilterTypes;
+
+            /// <summary>
+            ///     Constructor parametrizado
+            /// </summary>
+            public FiltersAttribute(params FilterType[] filterTypes)
+            {
+                FilterTypes = filterTypes;
+            }
+        }
+
+        /// <summary>
+        ///     Atributo de campo de modelo que determina que es un campo por el cual se puede ordenar su entidad
+        /// </summary>
+        public class SortableAttribute : Attribute
+        {
+            /// <summary>
+            ///     Constructor por defecto
+            /// </summary>
+            public SortableAttribute()
+            {
+            }
+        }
+
+        /// <summary>
+        ///     Atributo de campo de modelo que determina que es un campo por el cual se puede ordenar su entidad
+        /// </summary>
+        public class ImportAttribute : Attribute
+        {
+            /// <summary>
+            ///     Constructor por defecto
+            /// </summary>
+            public ImportAttribute()
+            {
+
+            }
+        }
+
+        public class ValidEnumAttribute : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                if (value == null)
+                {
+                    return ValidationResult.Success;
+                }
+
+                var type = value.GetType();
+
+                if (!(type.IsEnum && Enum.IsDefined(type, value)))
+                {
+                    return new ValidationResult(ErrorMessage ?? $"{value} is not a valid value for type {type.Name}");
+                }
+
+                return ValidationResult.Success;
+            }
+        }
+    }
+}
