cd6588f5d378f2ef92c42f57381838bdfb9546e1
meigemn <carloscc9297@gmail.com>
2026-04-14 18:14:26 +0200

    :sparkles: Added Literals to improve responses on UserService

---
+++ secondhand_car_backend/Utils/Literals.cs
@@ -0,0 +1,18 @@
+﻿namespace secondhand_car_backend.Utils
+{
+    public static class Literals
+    {
+        public static class UserMessages
+        {
+            public const string UserNotFound = "El usuario no existe.";
+            public const string InvalidPassword = "La contraseña es incorrecta.";
+            public const string EmailAlreadyRegistered = "Este email ya está en uso.";
+            public const string UserCreatedSuccess = "Usuario creado correctamente.";
+        }
+
+        public static class Auth
+        {
+            public const string InvalidToken = "Token no válido.";
+        }
+    }
+}
