f335128407ef5d01ce9cbccd15618d659bc00da6
meigemn <carloscc9297@gmail.com>
2026-04-14 15:48:32 +0200

    :sparkles: Added CreateEditRemoveResponseDto on Models->Dtos->ResponseDtos

---
+++ secondhand_car_backend/Models/Dtos/ResponseDtos/CreateEditRemoveResponseDto.cs
@@ -0,0 +1,24 @@
+﻿namespace secondhand_car_backend.Models.Dtos.ResponseDtos
+{
+    public class CreateEditRemoveResponseDto
+    {
+        public int Id { get; set; }
+        public bool Success { get; set; }
+        public List<string> Errors { get; set; }
+
+        #region Constructors
+
+        public CreateEditRemoveResponseDto()
+        {
+            Errors = new List<string>();
+        }
+
+        #endregion
+
+        public void IsSuccess(int id)
+        {
+            Id = id;
+            Success = true;
+        }
+    }
+}
