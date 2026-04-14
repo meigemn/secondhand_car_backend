b71286d7a48f5d06a0898b78a762be57bd7bae6d
meigemn <carloscc9297@gmail.com>
2026-04-14 15:47:29 +0200

    :sparkles: Added FilterDtos

---
+++ secondhand_car_backend/Models/Dtos/FilterDtos/GenericFilterDto.cs
@@ -0,0 +1,23 @@
+﻿using secondhand_car_backend.Utils;
+using System.ComponentModel;
+
+namespace shopping_list_backend.Models.Dtos.FilterDtos
+{
+    public class GenericFilterDto
+    {
+        public GenericFilterDto()
+        {
+            SorteablesFields = new List<string>();
+        }
+
+        public int Page { get; set; }
+
+        public string? SearchString { get; set; }
+
+        public string? OrderField { get; set; }
+
+        public IEnumerable<string> SorteablesFields { get; set; }
+
+        public IEnumerable<string> FilterablesFields { get; set; }
+    }
+}
