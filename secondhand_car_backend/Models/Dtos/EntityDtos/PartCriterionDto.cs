ca05738cea8ea8d1f7abe65dde20a6485cd3966f
meigemn <carloscc9297@gmail.com>
2026-04-17 14:48:52 +0200

    :sparkles: new PartCriterionDto. Models -> Dtos -> EntityDtos -> PartCriterionDto

---
+++ secondhand_car_backend/Models/Dtos/EntityDtos/PartCriterionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace secondhand_car_backend.Models.Dtos.EntityDtos
+{
+    public class PartCriterionDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsPositive { get; set; }
+    }
+}
