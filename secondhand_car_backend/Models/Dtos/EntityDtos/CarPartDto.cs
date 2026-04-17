d43c411aaf65998996ffcb55eb53964f5e525ba9
meigemn <carloscc9297@gmail.com>
2026-04-17 14:48:04 +0200

    :sparkles: new CarPartDto. Models -> Dtos -> EntityDtos -> CarPartDto

---
+++ secondhand_car_backend/Models/Dtos/EntityDtos/CarPartDto.cs
@@ -0,0 +1,13 @@
+﻿using secondhand_car_backend.Models.Entities;
+
+namespace secondhand_car_backend.Models.Dtos.EntityDtos
+{
+    public class CarPartDto
+    {
+        public int Id { get; set; }
+        public string PartName { get; set; }
+        public string Category { get; set; }
+        //Lista de dtos
+        public List<PartCriterionDto> Criteria { get; set; } = new();
+    }
+}
