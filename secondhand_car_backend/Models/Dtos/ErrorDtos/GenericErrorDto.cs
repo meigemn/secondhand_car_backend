e03b528e9d5f0abbd1d3b4af41c058db56e7af35
meigemn <carloscc9297@gmail.com>
2026-04-17 15:02:41 +0200

    :sparkels: new ErrorDto. Models -> Dtos ->ErrorDtos

---
+++ secondhand_car_backend/Models/Dtos/ErrorDtos/GenericErrorDto.cs
@@ -0,0 +1,20 @@
+﻿using secondhand_car_backend.Utils;
+
+namespace secondhand_car_backend.Models.Dtos.ErrorDtos
+{
+    public class GenericErrorDto
+    {
+        public ResponseCodes Id { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+
+        public GenericErrorDto()
+        {
+            // Inicializa a Ok (0) para indicar que no hay error
+            Id = ResponseCodes.Ok;
+            Location = string.Empty;
+            Description = string.Empty;
+        }
+
+    }
+}
