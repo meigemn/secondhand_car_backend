1803d06a9c10326a07488a0f3519bc1e47739706
meigemn <carloscc9297@gmail.com>
2026-04-17 13:20:41 +0200

    :sparkles: new CarParts entity on Models->Entities

---
+++ secondhand_car_backend/Models/Entities/CarParts.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace secondhand_car_backend.Models.Entities
+{
+    public class CarParts
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PartName { get; set; }
+        public string Category { get; set; }
+
+    }
+}
