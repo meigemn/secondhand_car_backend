020779e64e87c7c5a2761aba8d1e9df3cd66ca77
meigemn <carloscc9297@gmail.com>
2026-04-17 13:27:12 +0200

    :sparkles: new PartCriterion entity on Models->Entities

---
+++ secondhand_car_backend/Models/Entities/PartCriterion.cs
@@ -0,0 +1,13 @@
+﻿namespace secondhand_car_backend.Models.Entities
+{
+    public class PartCriterion
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsPositive { get; set; }
+
+        //claves ajenas
+        public int CarPartId { get; set; }
+        public CarParts CarParts { get; set; }
+    }
+}
