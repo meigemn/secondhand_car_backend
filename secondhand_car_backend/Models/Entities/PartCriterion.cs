7bcf786de95f5dc8c2b852239c2c125a11c041bf
meigemn <carloscc9297@gmail.com>
2026-04-17 14:25:31 +0200

    :sparkes: Added anotation on PartCriterion. Models -> Entities -> PartCriterion

--- secondhand_car_backend/Models/Entities/PartCriterion.cs
+++ secondhand_car_backend/Models/Entities/PartCriterion.cs
@@ -1,12 +1,17 @@
-﻿namespace secondhand_car_backend.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace secondhand_car_backend.Models.Entities
 {
     public class PartCriterion
-    {
+    {   [Key]
         public int Id { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public bool IsPositive { get; set; }
 
-        //claves ajenas
+        //FK
+        [Required]
         public int CarPartId { get; set; }
         public CarPart CarPart { get; set; }
     }
