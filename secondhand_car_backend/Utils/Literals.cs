b346ea581b66d3263ee3ac8278e9fb84876db161
meigemn <carloscc9297@gmail.com>
2026-04-17 13:57:45 +0200

    :technologist: Better Literals for UserService. Utils->Literals

--- secondhand_car_backend/Utils/Literals.cs
+++ secondhand_car_backend/Utils/Literals.cs
@@ -20,5 +20,16 @@
             public const string InvalidCredentials = "Credenciales inválidas";
 
         }
+        public static class UserServiceErrors
+        {
+            public const string LoginError = "Error en el Login. Services->UserService->Login";
+            public const string GetAllError = "Error en GetAll. Services->UserService->GetAll";
+            public const string GetByIdError = "Error en GetById. Services->UserService->GetById";
+            public const string CreateAdminError = "Error en CreateAdmin. Services->UserService->CreateAdmin";
+            public const string RemoveError = "Error en Remove. Services->UserService->Remove";
+            public const string UpdateError = "Error en Update. Services->UserService->Update";
+
+
+        }
     }
 }
