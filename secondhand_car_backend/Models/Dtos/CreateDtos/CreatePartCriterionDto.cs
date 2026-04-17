5a104d6b24ac529647e98a7539cbcbbda84cf85b
meigemn <carloscc9297@gmail.com>
2026-04-17 14:36:04 +0200

    :sparkles: new CreatePartCriterionDto. Models -> CreateDtos -> CreatePartCriterionDto

---
+++ secondhand_car_backend/Models/Dtos/CreateDtos/CreatePartCriterionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace secondhand_car_backend.Models.Dtos.CreateDtos
+{
+    public class CreatePartCriterionDto
+    {
+        public string Description { get; set; }
+        public bool IsPositive { get; set; }
+        public int CarPartId { get; set; }
+    }
+}
