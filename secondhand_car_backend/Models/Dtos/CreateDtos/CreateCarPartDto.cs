d095976fea7dcd86ab6f198fa5fb545c0a5df6c0
meigemn <carloscc9297@gmail.com>
2026-04-17 14:34:10 +0200

    :sparkles: new CreateCarPartDto. Models -> CreateDtos -> CreateCarPartDto

---
+++ secondhand_car_backend/Models/Dtos/CreateDtos/CreateCarPartDto.cs
@@ -0,0 +1,8 @@
+﻿namespace secondhand_car_backend.Models.Dtos.CreateDtos
+{
+    public class CreateCarPartDto
+    {
+        public string PartName { get; set; }
+        public string Category { get; set; }
+    }
+}
